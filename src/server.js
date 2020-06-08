092dccd7bb9e61627e21e4c2d4301cbe7db5c6a0
Fauez Passos <fauez.passos@outlook.com>
2020-06-08 14:08:45 -0300

    config template engine (nunjucks)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
-server.get('/', (request, response) => {
-  return response.json({ message: 'OK' });
+// config template engine
+nunjucks.configure('src/viewer', {
+  express: server,
+  noCache: true
+});
+
+// config static files
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+server.get('/create-point', (req, res) => {
+  res.render('create-point.html');
 });
 
 server.listen(3000);
