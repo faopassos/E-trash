f3c6d17b09a246c8e76fa8b39ab58d14f5356d94
Fauez Passos <fauez.passos@outlook.com>
2020-06-17 09:44:49 -0300

    insert data from form

--- src/server.js
+++ src/server.js
@@ -16,12 +16,54 @@ nunjucks.configure('src/views', {
 // config static files
 server.use(express.static('public'));
 
+server.use(express.urlencoded({ extended: true }));
+
 server.get('/', (req, res) => {
   return res.render('index.html');
 });
 
 server.get('/create-point', (req, res) => {
+  // console.log(req.query);
+
   return res.render('create-point.html');
+});
+
+server.post('/savepoint', (req, res) => {
+  
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?, ?, ?, ?, ?, ?, ?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ];
+
+  function afterInsertData(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log('Cadastrado com sucesso');
+    console.log(this);
+
+    return res.send('ok');
+  }
+
+  db.run(query, values, afterInsertData);
 });
 
 server.get('/search', (req, res) => {
